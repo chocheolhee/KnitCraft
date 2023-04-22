60747367a292b7adc8b735dd0458102e0b641e84
chocheolhee <whcjfgml92@naver.com>
2023-04-22 17:27:53 +0900

    toyProject setting commit added

---
+++ src/main/java/Toy/KnitCraft/KnitCraftApplication.java
@@ -0,0 +1,13 @@
+package Toy.KnitCraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnitCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnitCraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Toy/KnitCraft/KnitCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package Toy.KnitCraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnitCraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
