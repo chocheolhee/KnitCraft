42fc470c3e7fd22b0ec78460a7249a6c7c97ff62
chocheolhee <whcjfgml92@naver.com>
2023-04-25 21:22:13 +0900

    errorResponse class controllerAdvice added

--- src/test/java/Toy/KnitCraft/controller/PostControllerTest.java
+++ src/test/java/Toy/KnitCraft/controller/PostControllerTest.java
@@ -1,4 +1,33 @@
-import static org.junit.jupiter.api.Assertions.*;
+package Toy.KnitCraft.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
 class PostControllerTest {
-  
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("/posts 요청 시 DB에 값이 저장된다.")
+    void test1() throws Exception {
+        // expected
+        mockMvc.perform(post("/posts"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
+                .andExpect(jsonPath("$.validation.title").value("타이틀을 입력하세요."))
+                .andDo(print());
+    }
 }
