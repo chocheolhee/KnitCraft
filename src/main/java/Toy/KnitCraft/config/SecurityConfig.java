9be61867831540bc31af13eb4f931a5cd704bccb
chocheolhee <whcjfgml92@naver.com>
2023-05-16 21:22:17 +0900

    springSecurity setting

---
+++ src/main/java/Toy/KnitCraft/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package Toy.KnitCraft.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers("/favicon.ico", "/error");
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests()
+                .requestMatchers("/auth/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
--- src/main/java/Toy/KnitCraft/controller/AuthController.java
+++ src/main/java/Toy/KnitCraft/controller/AuthController.java
@@ -4,6 +4,7 @@ import Toy.KnitCraft.request.Signup;
 import Toy.KnitCraft.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+
+    @GetMapping("/auth/login")
+    public String login() {
+        return "login page";
+    }
 
     @PostMapping("/auth/signup")
     public void signup(@RequestBody Signup signup) {
